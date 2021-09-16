235191185cd2729b6d620e938e5028eb5b1dbcd2
Victor <vitelafr@gmail.com>
2021-09-15 21:56:13 -0300

    testes

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function soma(num1, num2) {
+  return num1 + num2;
+}
+
+function subtracao(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplicacao(num1, num2) {
+  return num1 * num2;
+}
+
+function divisao(num1, num2) {
+  return num1 / num2;
+}
+
+exports.multiplicacao = multiplicacao;
+exports.divisao = divisao;
+exports.soma = soma;
+exports.subtracao = subtracao;
---
+++ calculator.test.js
@@ -0,0 +1,16 @@
+//import { expect, test } from '@jest/globals';
+//import * as calculator from './calculator.js';
+
+const calculator = require("./calculator");
+
+test("testando soma de 1+1", () => {
+  expect(calculator.soma(1, 1)).toBe(2);
+});
+
+test("testar divisao de 42/6", () => {
+  expect(calculator.divisao(42, 6)).toBe(7);
+});
+
+test("testar soma de 5+5", () => {
+  expect(calculator.soma(5, 5)).toBe(11);
+});
